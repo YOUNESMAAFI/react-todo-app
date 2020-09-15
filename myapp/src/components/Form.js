7bf7989b5aab5a0c5ff62190ffe556f51279d129
YOUNESMAAFI <younesmaafi@gmail.com>
2020-09-15 08:00:25 +0100

    MVP

--- myapp/src/components/Form.js
+++ myapp/src/components/Form.js
@@ -18,22 +18,10 @@ const Form = ({ setInputText, todos, setTodos, inputText }) => {
   };
   return (
     <form>
-      <input
-        value={inputText}
-        onChange={inputTextHandler}
-        type="text"
-        className="todo-input"
-      />
+      <input value={inputText} onChange={inputTextHandler} type="text" />
       <button onClick={submitTodoHandler} className="todo-button" type="submit">
         <i className="fas fa-plus-square"></i>
       </button>
-      <div className="select">
-        <select name="todos" className="filter-todo">
-          <option value="all">All</option>
-          <option value="completed">Completed</option>
-          <option value="uncompleted">Uncompleted</option>
-        </select>
-      </div>
     </form>
   );
 };
